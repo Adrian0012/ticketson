1851422d33361b81766abaf09dee9cb3fd50ea50
Adrian0012 <adrian.g87@hotmail.com>
2022-12-03 23:29:38 +0000

    update wallet cards - use paint

--- ticketson/lib/modules/wallets/screens/wallets_list.dart
+++ ticketson/lib/modules/wallets/screens/wallets_list.dart
@@ -109,19 +109,23 @@ class WalletItem extends StatefulWidget {
 class _WalletItemState extends State<WalletItem> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: (() {
         Beamer.of(context)
             .beamToNamed(Routes.tickets, data: {'wallet': widget.wallet});
       }),
       child: Container(
+        width: size.width,
+        height: size.height / 5.5,
         decoration: BoxDecoration(
           color: Palette.primaryColor.withOpacity(0.2),
-          borderRadius: BorderRadius.circular(16.0),
-          border: Border.all(
-            width: 3.0,
-            color: Palette.secondaryColor.withOpacity(0.5),
-          ),
+
+          // borderRadius: BorderRadius.circular(16.0),
+          // border: Border.all(
+          //   width: 1.0,
+          //   color: Palette.secondaryColor.withOpacity(0.5),
+          // ),
           boxShadow: [
             BoxShadow(
               blurRadius: 16,
@@ -130,55 +134,103 @@ class _WalletItemState extends State<WalletItem> {
             ),
           ],
         ),
-        width: 360,
-        height: 200,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  const Icon(
-                    Icons.wallet_rounded,
-                    size: 25.0,
-                    color: Palette.primaryColor,
-                  ),
-                  const Padding(padding: EdgeInsets.only(right: 4.0)),
-                  Text(
-                    widget.wallet.name,
-                    style: const TextStyle(
-                      color: Palette.white,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 0.2,
+        child: CustomPaint(
+          painter: RPSCustomPainter(),
+          // child: Container(
+          //   decoration: BoxDecoration(
+          //     color: Palette.primaryColor.withOpacity(0.2),
+          //     // borderRadius: BorderRadius.circular(16.0),
+          //     border: Border.all(
+          //       width: 3.0,
+          //       color: Palette.secondaryColor.withOpacity(0.5),
+          //     ),
+          //     boxShadow: [
+          //       BoxShadow(
+          //         blurRadius: 16,
+          //         spreadRadius: 16,
+          //         color: Colors.black.withOpacity(0.1),
+          //       ),
+          //     ],
+          //   ),
+          //   width: size.width,
+          //   height: size.height / 5.5,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.wallet_rounded,
+                      size: 25.0,
+                      color: Palette.primaryColor,
                     ),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    widget.wallet.ticketCount.toString(),
-                    style: const TextStyle(
-                      color: Palette.secondaryColor,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w900,
+                    const Padding(padding: EdgeInsets.only(right: 4.0)),
+                    Text(
+                      widget.wallet.name,
+                      style: const TextStyle(
+                        color: Palette.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.2,
+                      ),
                     ),
-                  ),
-                  const Padding(padding: EdgeInsets.all(2.0)),
-                  const Icon(
-                    Icons.airplane_ticket,
-                    size: 30.0,
-                    color: Palette.primaryColor,
-                  )
-                ],
-              ),
-            ],
+                  ],
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      widget.wallet.ticketCount.toString(),
+                      style: const TextStyle(
+                        color: Palette.secondaryColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(2.0)),
+                    const Icon(
+                      Icons.airplane_ticket,
+                      size: 30.0,
+                      color: Palette.primaryColor,
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint0 = Paint()
+      ..color = Palette.secondaryColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1;
+
+    Path path0 = Path();
+    path0.moveTo(0, size.height);
+    path0.lineTo(size.width, size.height);
+    path0.lineTo(size.width, 0);
+    path0.lineTo(size.width * 0.6480519, 0);
+    path0.lineTo(size.width * 0.5989610, size.height * 0.2057931);
+    path0.lineTo(size.width * 0.3892208, size.height * 0.2076552);
+    path0.lineTo(size.width * 0.3256364, size.height * 0.0014483);
+    path0.lineTo(0, 0);
+    path0.lineTo(0, size.height);
+    path0.close();
+
+    canvas.drawPath(path0, paint0);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
